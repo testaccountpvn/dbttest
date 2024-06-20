bef319c44f93bc8491d72034d8a6229560dc4ce7
ashok@dqlabs.ai <ashok@dqlabs.ai>
2024-06-20 04:02:36 +0000

    updates

--- tests/consolidated_view_test.sql
+++ tests/consolidated_view_test.sql
@@ -1,16 +1,6 @@
--- tests/not_null_profit_test.sql
-
-WITH validation AS (
-    SELECT
-        profit as tested_column
-    FROM {{ ref('consolidated_vw') }}
-),
-
-errors AS (
-    SELECT *
-    FROM validation
-    WHERE tested_column IS NULL
-)
-
-SELECT COUNT(*)
-FROM errors
+select
+    property_id,
+    sum(rental_income) as total_amount
+from {{ ref('consolidated_vw') }}
+group by 1
+having total_amount > 0
