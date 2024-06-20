19faa0b7b1d30e8bea49f0ea9d426ccdec718212
ashok@dqlabs.ai <ashok@dqlabs.ai>
2024-06-20 03:39:59 +0000

    update

--- tests/consolidated_view_test.sql
+++ tests/consolidated_view_test.sql
@@ -1,6 +1,16 @@
-select
-    property_id,
-    count(profit) as total_amount
-from {{ ref('consolidated_vw') }}
-group by 1
-having total_amount > 0
+-- tests/not_null_profit_test.sql
+
+WITH validation AS (
+    SELECT
+        profit as tested_column
+    FROM {{ ref('financial_performance') }}
+),
+
+errors AS (
+    SELECT *
+    FROM validation
+    WHERE tested_column IS NULL
+)
+
+SELECT COUNT(*)
+FROM errors
