5f29fb10f6c7788723e5a2839217a4ff7516866a
ashok@dqlabs.ai <ashok@dqlabs.ai>
2024-08-05 08:31:26 +0000

    updates

---
+++ models/redshift_models/financial_performance.sql
@@ -0,0 +1,4 @@
+with financial_performance_vw as (
+    Select * FROM main.dqlabs.financial_performance_table
+)
+select * from financial_performance_vw
---
+++ models/redshift_models/marketing_data.sql
@@ -0,0 +1,4 @@
+with marketing_data_vw as (
+    Select * FROM main.dqlabs.marketing_data_table
+)
+select * from marketing_data_vw
