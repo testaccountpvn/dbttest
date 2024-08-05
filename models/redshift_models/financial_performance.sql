f343f9ec2a49534021f80ccfdd5ba3aecc907cf9
ashok@dqlabs.ai <ashok@dqlabs.ai>
2024-08-05 08:34:31 +0000

    udpates

--- models/redshift_models/financial_performance.sql
+++ models/redshift_models/financial_performance.sql
@@ -1,4 +1,4 @@
 with financial_performance_vw as (
-    Select * FROM main.dqlabs.financial_performance_table
+    Select * FROM dbt_dqlabs.financial_performance_table
 )
 select * from financial_performance_vw
--- models/redshift_models/property_information.sql
+++ models/redshift_models/property_information.sql
@@ -1,4 +1,4 @@
 with property_information_vw as (
-    Select * FROM main.dqlabs.property_information_table
+    Select * FROM dbt_dqlabs.property_information_table
 )
 select * from property_information_vw
